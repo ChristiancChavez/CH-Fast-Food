f40fbdaa344ac0c9c7492e44ffbfefd0f071eab4
Christian Chavez <christiancchavez10@hotmail.com>
2020-05-01 15:59:12 -0500

    create folders and install proptypes

---
+++ src/Components/OptionsHeader/OptionsHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const OptionsHeader = () => (
+    <div>
+        <Link to="">
+            <i class="fas fa-users"></i>
+        </Link>
+    </div>
+)
+
+export default OptionsHeader;
